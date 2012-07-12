1ec83c4d6dc4755f169ff7ed766aa083008c14e5
Peter Inglesby <peter.inglesby@gmail.com>
2012-07-12 21:20:39 +0100

    Initial interface

---
+++ game.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Board(object):
+    def __init__(self):
+        self.pos_map = defaultdict(set)
+        self.tok_map = {}
+
+    def move(self, turn, pos1, pos2):
+        self.pos_map[pos1].add(turn)
+        self.pos_map[pos2].add(turn)
+        self.tok_map[turn] = (pos1, pos2)
+
+class Game(object):
+    def __init__(self):
+        self.b = Board()
+
+    def play(self):
+        for turn in range(1, 10):
+            pos1, pos2 = self.get_move(i)
+            self.board.move(turn, pos1, pos2)
+            self.display()
+            if board.has_cycle():
+                choices = board.get_cycle_breaker()
+                choice = self.get_user_choice(choices)
+                board.collapse(choice)
+                self.display()
+            board.check_winner()
+
+
+if __name__ == '__main__':
+    b = Board()
+    b.move(1, 2, 3)
+    print b.__dict__
